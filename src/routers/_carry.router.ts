e11c79a79821a68769a491b7d73624af9177fb79
Ryan Waite <ryanwaite28@gmail.com>
2023-05-20 02:02:03 -0400

    changed csrf middleware

--- src/routers/_carry.router.ts
+++ src/routers/_carry.router.ts
@@ -8,6 +8,7 @@ import { corsMiddleware, corsMobileMiddleware } from '../utils/constants.utils';
 import { DeliveriesRouter } from './deliveries.router';
 import { UsersRouter } from './users.router';
 import { CommonRouter } from './common.router';
+import { CsrfProtectionMiddleware } from 'src/middlewares/csrf.middleware';
 
 
 export const CarryRouter: Router = Router({ mergeParams: true });
@@ -34,12 +35,13 @@ const enable_cors = (
 
 console.log({ enable_cors });
 
-if (enable_cors) {
+// if (enable_cors) {
 
   CarryWebRouter.use(corsMiddleware);
 
-}
+// }
 
+CarryWebRouter.use(CsrfProtectionMiddleware);
 CarryWebRouter.use('/users', UsersRouter);
 CarryWebRouter.use('/deliveries', DeliveriesRouter);
 CarryWebRouter.use('/common', CommonRouter);
--- src/routers/utils.router.ts
+++ src/routers/utils.router.ts
@@ -1,16 +1,21 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import {
   XSRF_PROTECTED_2
 } from '../guards/xsrf.guard';
 import { UtilsRequestHandler } from '../handlers/utils.handler';
+import { HttpStatusCode } from 'src/enums/http-codes.enum';
 
 export const UtilsRouter: Router = Router({ mergeParams: true });
+
+UtilsRouter.get('/get-csrf-token', (request: Request, response: Response) => {
+  return response.status(HttpStatusCode.OK).json({ message: `Admit One` });
+});
 
 UtilsRouter.get('/get-xsrf-token', UtilsRequestHandler.get_xsrf_token);
 UtilsRouter.get('/get-xsrf-token-pair', UtilsRequestHandler.get_xsrf_token_pair);
 
-UtilsRouter.post('/get-google-api-key', XSRF_PROTECTED_2, UtilsRequestHandler.get_google_maps_key);
-UtilsRouter.post('/get-stripe-public-key', XSRF_PROTECTED_2, UtilsRequestHandler.get_stripe_public_key);
+UtilsRouter.post('/get-google-api-key', UtilsRequestHandler.get_google_maps_key);
+UtilsRouter.post('/get-stripe-public-key', UtilsRequestHandler.get_stripe_public_key);
 
 UtilsRouter.put('/get-google-api-key', UtilsRequestHandler.get_google_maps_key);
 UtilsRouter.put('/get-stripe-public-key', UtilsRequestHandler.get_stripe_public_key);
